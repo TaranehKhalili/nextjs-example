7c1266a5061b6387a4e835d16ecb017184201db9
Taraneh Khalili <taraneh.khalili@noah-labs.com>
2025-10-05 10:37:53 +0200

    Add metadata to Header and Contact components; apply Poppins font to Header

--- app/app/components/Header.tsx
+++ app/app/components/Header.tsx
@@ -1,9 +1,24 @@
 import Image from "next/image";
 import Link from "next/link";
+import { Poppins } from "next/font/google";
+import { Metadata } from "next";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
+
+export const metadata: Metadata = {
+  title: "Header",
+  description: "Header",
+};
 
 export default function Header() {
   return (
-    <header className="flex justify-between items-center bg-gray-300 text-white p-4 fixed top-0 w-full h-16">
+    <header
+      className={`flex justify-between items-center bg-gray-300
+                 text-white p-4 fixed top-0 w-full h-16 ${font.className}`}
+    >
       <Image
         src="https://picsum.photos/100/100"
         alt="logo"
@@ -11,7 +26,7 @@ export default function Header() {
         height={40}
       />
       <nav>
-        <ul className="flex gap-4">
+        <ul className="flex gap-4 font-bold">
           <Link href="/">Home</Link>
           <Link href="/about">About</Link>
           <Link href="/contact">Contact</Link>
